7f1097a667b9a42be1afeca1cb2daf6193206202
S m, Aruna <aruna.mohan@walmartlabs.com>
2019-11-08 15:20:20 +0530

    Add CLI for produce-consume application
    Signed-off-by: S m, Aruna <aruna.mohan@walmartlabs.com>

---
+++ cli/src/cli_error.rs
@@ -0,0 +1,53 @@
+// Copyright 2019 Walmart Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+use std::string::ToString;
+use std::error::Error;
+use std::fmt;
+
+#[derive(Debug)]
+pub(crate) struct CliError {
+    inner: String,
+}
+
+impl std::fmt::Display for CliError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "CliError: {:?}", &self.inner)
+    }
+}
+
+impl From<String> for CliError {
+    fn from(inner: String) -> CliError {
+        CliError { inner }
+    }
+}
+
+impl From<&'static str> for CliError {
+    fn from(inner: &'static str) -> CliError {
+        CliError {
+            inner: inner.to_string(),
+        }
+    }
+}
+
+impl Error for CliError {
+    fn description(&self) -> &str {
+        &self.inner
+    }
+
+    fn cause(&self) -> Option<&dyn Error> {
+        // Generic error, underlying cause isn't tracked
+        None
+    }
+}
---
+++ cli/src/main.rs
@@ -0,0 +1,86 @@
+// Copyright 2019 Walmart Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+extern crate clap;
+
+mod produce_consume;
+mod cli_error;
+
+use clap::App;
+use clap::Arg;
+
+fn main() {
+    let matches = App::new("pc-cli")
+        .author("Walmart Inc.")
+        .version("1.0")
+        .about("Sample sawtooth-sabre smart contract produce-consume cli")
+        .arg(
+            Arg::with_name("command")
+                .short("C")
+                .long("command")
+                .help("Command either PRODUCE or CONSUME")
+                .takes_value(true)
+                .required(true)
+                .value_names(&["PRODUCE", "CONSUME"]),
+        )
+        .arg(
+            Arg::with_name("identifier")
+                .short("I")
+                .long("identifier")
+                .help("Identifier of the produced item")
+                .takes_value(true)
+                .required(true),
+        )
+        .arg(
+            Arg::with_name("quantity")
+                .short("Q")
+                .long("quantity")
+                .help("Quantity of the produced item")
+                .takes_value(true)
+                .required(true),
+        )
+        // Optional arguments, for which the default values are used
+        .arg(
+            Arg::with_name("url")
+                .short("U")
+                .long("url")
+                .help("URL of the validator to send the request")
+                .takes_value(true)
+                .required(false),
+        )
+        .arg(
+            Arg::with_name("key")
+                .short("K")
+                .long("key")
+                .help("Key used for signing the transaction")
+                .takes_value(true)
+                .required(false),
+        )
+        .get_matches();
+
+    // This is a CLI application, an irrecoverable error occurs if the input is not good
+    let command = matches.value_of("command").unwrap();
+    let identifier = matches.value_of("identifier").unwrap();
+    let quantity = matches.value_of("quantity").unwrap();
+    let url = matches.value_of("url").unwrap_or("http://localhost:8008");
+    let key = matches.value_of("key").unwrap_or("/etc/sawtooth/keys/validator.priv");
+
+    match produce_consume::submit_payload(command, identifier, quantity, url, key) {
+        Ok(_) => println!("Successfully submitted the transaction"),
+        Err(err) => {
+            println!("Unable to submit the transaction {}", err);
+            std::process::exit(1);
+        },
+    }
+}
